7e0c61fca832a9e9f38f44bded4f1fed787c9efb
Ad5001 <ad5001yt@gmail.com>
2016-05-13 20:28:16 +0200

    Will work when I will have finish http://mc-pe.ga !

--- src/BoxOfDevs/Shortener/Main.php
+++ src/BoxOfDevs/Shortener/Main.php
@@ -3,16 +3,35 @@ namespace BoxOfDevs\Shortener ;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
  use pocketmine\Player;
 
 
-class Main extends PluginBase{
+class Main extends PluginBase implements Listener{
 public function onEnable(){
-// $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    include("http://mc-pe.ga/shortener.php");
+$this->getServer()->getPluginManager()->registerEvents($this, $this);
  }
-public function onLoad(){
+public function onChat(PlayerChatEvent $event){
+    $words = [];
+    foreach(explode(" ", $event->getMessage()) as $word) {
+        if (filter_var($word, FILTER_VALIDATE_URL)) {
+            $file_headers = get_headers($word);
+            if($file_headers[0] == 'HTTP/1.1 404 Not Found') {
+                if(parse_url($word, PHP_URL_SCHEME) === "http", parse_url($word, PHP_URL_SCHEME) === "https") {
+                    $surl = new SUrl($word, $this);
+                    $word = $surl->getNewUrl();
+                }
+            }
+        }
+        array_push($words, $word);
+    }
+    $event->setMessage(implode(" ", $words)):
+}
+public function auth() {
+    $this->getConfig()->get("TokenAuth");
 }
  public function onCommand(CommandSender $issuer, Command $cmd, $label, array $params){
 switch($cmd->getName()){
